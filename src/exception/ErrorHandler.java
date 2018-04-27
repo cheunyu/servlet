82ad5099d884a893ecfd5d8d4416b0777bf51fb5
cheunyu <cheunyufoo@gmail.com>
2018-04-27 22:59:48 +0800

    error exception

---
+++ src/exception/ErrorHandler.java
@@ -0,0 +1,57 @@
+package exception;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author:cheunyu
+ * @date:2018/4/27 21:51
+ */
+public class ErrorHandler extends HttpServlet {
+
+    /**
+     * @description:Serlvet初始化方法
+     * @param:[]
+     * @return:void
+     */
+    public void init() {
+        System.out.println("ErrorHandler加载");
+    }
+
+    /**
+     * @description:处理get请求
+     * @param:[]
+     * @return:void
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+        System.out.println("doGet");
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        String servletName = (String) request.getAttribute("javax.servlet.error.servlet_name");
+        String requestUri = (String) request.getAttribute("javax.servlet.error.request_uri");
+        System.out.println("错误代码--->" + statusCode);
+        System.out.println("Servlet名称--->" + servletName);
+        System.out.println("请求Uri--->" + requestUri);
+    }
+    
+    /**
+     * @description:处理post请求
+     * @param:[]
+     * @return:void
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost");
+        doGet(request, response);
+    }
+
+    /**
+     * @description:Servlet销毁方法
+     * @param:[]
+     * @return:void
+     */
+    public void destory() {
+
+    }
+}
--- src/servlet/FirstServlet.java
+++ src/servlet/FirstServlet.java
@@ -1,9 +1,12 @@
 package servlet;
 
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author:cheunyu
@@ -21,6 +24,17 @@ public class FirstServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
         String uname = request.getParameter("username");
         System.out.println(uname);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("进入doPost方法");
+        try {
+            response.sendRedirect("/redirect.html");
+//            RequestDispatcher dispatcher = request.getRequestDispatcher("/forward.html");
+//            dispatcher.forward(request, response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void destory() {
